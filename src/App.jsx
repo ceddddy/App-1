ba126ce4e2967cd55a3784202e0a04a390258da9
Ceddy <ceddddyadvance@gmail.com>
2022-03-24 02:09:12 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,34 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          
-          <div class="container-fluid">
-
-            <a class="navbar-brand" href="#">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/#">
               My App
             </a>
 
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                <li class="nav-item">
-
-                  <a class="nav-link" href="#">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="/#">
                     Home
                   </a>
-
                 </li>
-
-                <li class="nav-item">
-
-                  <a class="nav-link" href="#">
-                    House
-                  </a>
-
-                </li>
-
               </ul>
             </div>
-
           </div>
-
         </nav>
       </React.Fragment>
     );
