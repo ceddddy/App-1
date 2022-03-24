02734ceec988d9b67996bf8ca3542d6d83540ebb
Ceddy <ceddddyadvance@gmail.com>
2022-03-24 13:50:46 +0200

    added table with customers' data

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,8 +1,72 @@
 import React, { Component } from "react";
-class MainContent extends Component {
-  render() {
-    return <div>Hello From Main Content</div>;
-  }
-}
 
-export default MainContent;
+export default class MainContent extends Component {
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+    customers: [
+      {
+        id: 1,
+        name: "Scott",
+        phone: "123-456",
+        address: { city: "New Dehli" },
+      },
+      {
+        id: 2,
+        name: "Jones",
+        phone: "982-014",
+        address: { city: "New Jersey" },
+      },
+      { id: 3, name: "Allen", phone: "889-921", address: { city: "London" } },
+      { id: 4, name: "James", phone: "552-901", address: { city: "Berlin" } },
+      { id: 5, name: "John", phone: "781-778", address: { city: "New York" } },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.pageTitle}
+          <span className="badge badge-secondary m-2">
+            {this.state.customersCount}
+          </span>
+
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
+        </h4>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.customers.map((cust) => {
+              return (
+                <tr key={cust.id}>
+                  <td>{cust.id}</td>
+                  <td>{cust.name}</td>
+                  <td>{cust.phone}</td>
+                  <td>{cust.address.city}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  //Executes when the user clicks on Refresh button
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 7,
+    });
+  };
+}
