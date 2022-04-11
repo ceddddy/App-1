673dbe78f90481b36db4f43742d684d3128f6b5b
Ceddy <ceddddyadvance@gmail.com>
2022-04-11 15:46:15 +0200

    added props and bootstrap cards

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -4,22 +4,29 @@ export default class ShoppingCart extends Component {
   state = {
     products: [
       { id: 1, productName: "iPhone", price: 8900, quantity: 1 },
-      { id: 1, productName: "Sony Camera", price: 4500, quantity: 2 },
-      { id: 1, productName: "iPad Pro", price: 12400, quantity: 4 },
-      { id: 1, productName: "Samsung QLED TV", price: 7745, quantity: 3 },
-      { id: 1, productName: "Xbox", price: 7780, quantity: 5 },
-      { id: 1, productName: "Dell Monitor", price: 880, quantity: 6 },
+      { id: 2, productName: "Sony Camera", price: 4500, quantity: 2 },
+      { id: 3, productName: "iPad Pro", price: 12400, quantity: 4 },
+      { id: 4, productName: "Samsung QLED TV", price: 7745, quantity: 3 },
+      { id: 5, productName: "Xbox", price: 7780, quantity: 5 },
+      { id: 6, productName: "Dell Monitor", price: 880, quantity: 6 },
     ],
   };
 
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <h4>Shopping Cart</h4>
 
-        <div>
-          {this.state.products.map(() => {
-            return <Product />;
+        <div className="row">
+          {this.state.products.map((prod) => {
+            return (
+              <Product
+                key={prod.id}
+                id={prod.id}
+                productName={prod.productName}
+                price={prod.price}
+              />
+            );
           })}
         </div>
       </div>
